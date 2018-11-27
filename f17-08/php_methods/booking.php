d219b421787eae9748662363decbb525564ccee3
Max <prochilm1@hawkmail.newpaltz.edu>
2018-11-27 14:38:05 -0500

    booking.php connects to database

--- f17-08/php_methods/booking.php
+++ f17-08/php_methods/booking.php
@@ -7,16 +7,11 @@
         error_reporting(0);
 
         //Current fields being inserted into for current table
-        //$email = mysqli_real_escape_string($conn, $_POST['email']);
         $bookedroom = mysqli_real_escape_string($conn, $_POST['bookedroom']);
         $starttime = mysqli_real_escape_string($conn, $_POST['starttime']);
         $endtime = mysqli_real_escape_string($conn, $_POST['endtime']);
        
         //Error handelers - Check for empty fields
-        //if (empty($email)) {
-        //    header("Location: ../register.php?emailfield=empty");
-        //    exit();
-        //}
         if (empty($bookedroom)) {
             header("Location: ../scheduler.php?bookedroomfield=empty");
             exit();
@@ -32,27 +27,15 @@
 
         //If no problems insert the user into the database
         else {
-            //Try and findif the email is already in use
-        //    $sql = "SELECT user_id FROM booking WHERE user_email = '$email'";
-        //    $result = mysqli_query($conn, $sql);
-        //    $count = mysqli_num_rows($result);
-        //    if ($count > 0) {
-        //        $_SESSION['email_error'] = true;
-        //        header("Location: ../register.php?email=used");
-        //        exit();
-        //    }
 
             //Must be "INSERT INTO..." then the name of the table being inserted into
             $sql = "INSERT INTO booking (bookedroom, starttime, endtime)
-            VALUES ({$bookedroom}', '{$starttime}', '{$endtime}');";
+            VALUES ('{$bookedroom}', '{$starttime}', '{$endtime}');";
 
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-         //   $_SESSION['login_email'] = $email;
-         //   $_SESSION['login_bookedroom'] = $bookedroom;
-
-            header("Location: /p/f17-08/v20/index.php");
+            
+            header("Location: ../scheduler.php");
 
             exit();
         }
